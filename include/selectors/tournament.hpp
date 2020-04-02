d06cc70ba813d551d73c0094f4138847311bb897
Axel Cocat <ax.cocat@gmail.com>
2020-04-02 22:35:35 +0200

    Finished tournament

--- include/selectors/tournament.hpp
+++ include/selectors/tournament.hpp
@@ -9,45 +9,62 @@ namespace Selector {
 template <typename T>
 class Tournament {
 public:
-    Tournament(int m) : m(m) {}
+    Tournament(int nbParticipants, int tournamentRounds)
+        : nbParticipants(nbParticipants), tournamentRounds(tournamentRounds) {}
 
-    int getM() { return this->m; }
-    void setM(int m) { this->m = m; }
+    int getNbParticipants() { return this->nbParticipants; }
+    void setNbParticipants(int nbParticipants) {
+        this->nbParticipants = nbParticipants;
+    }
+
+    int getTournamentRounds() { return this->tournamentRounds; }
+    void setTournamentRounds(int tournamentRounds) {
+        this->tournamentRounds = tournamentRounds;
+    }
 
     std::vector<T> operator()(const std::vector<T>& pop,
                               const std::vector<float>& ratings) const {
-        std::vector<T*> participants;
-        size_t n = pop.size();
-        assert(n == ratings.size());
-        assert(n >= this->m);
-
-        participants.reserve(this->m);
-        for (int i = 0; i < this->m; ++i) {
-            T* randParticipant;
-            do {
-                randParticipant = (T*)&pop[Random::getInt(0, n - 1)];
-            } while (std::find(participants.begin(), participants.end(),
-                               randParticipant) != participants.end());
-            participants.push_back(randParticipant);
-        }
-
         std::vector<T> selected;
-        T* best = nullptr;
-        float bestRating = 0.0f;
-        for (int i = 0; i < this->m; ++i) {
-            if (best == nullptr || ratings[i] > bestRating) {
-                best = participants[i];
-                bestRating = ratings[i];
+        std::vector<int> selectedIndices;
+
+        std::vector<int> participants;
+        participants.reserve(this->nbParticipants);
+
+        while (participants.size() != this->nbParticipants) {
+            participants.clear();
+            size_t n = pop.size();
+            assert(n == ratings.size());
+            assert(n >= this->nbParticipants);
+
+            for (int i = 0; i < this->nbParticipants; ++i) {
+                int randParticipant;
+                do {
+                    randParticipant = Random::getInt(0, n - 1);
+                } while (std::find(participants.begin(), participants.end(),
+                                   randParticipant) != participants.end());
+                participants.push_back(randParticipant);
+            }
+
+            int best = -1;
+            float bestRating = 0.0f;
+            for (int i = 0; i < this->nbParticipants; ++i) {
+                if (best == -1 || ratings[participants[i]] > bestRating) {
+                    best = participants[i];
+                    bestRating = ratings[participants[i]];
+                }
+            }
+            if (best != -1) {
+                selected.push_back(pop[best]);
+                selectedIndices.push_back(best);
             }
         }
-        if (best != nullptr) {
-            selected.push_back(*best);
-        }
+
         return selected;
     }
 
 private:
-    int m;
+    int nbParticipants;
+    int tournamentRounds;
 };
 
 } // namespace Selector
