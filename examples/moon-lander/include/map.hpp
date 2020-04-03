58ce4c69463dfb34f99be12778b800e6db6165ea
Axel Cocat <ax.cocat@gmail.com>
2020-04-03 09:54:48 +0200

    Added moonlander map

---
+++ examples/moon-lander/include/map.hpp
@@ -0,0 +1,4 @@
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+std::vector<std::array<sf::Vertex, 2>> buildMap();
--- examples/moon-lander/src/main.cpp
+++ examples/moon-lander/src/main.cpp
@@ -1,7 +1,14 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
+
+#include "map.hpp"
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    sf::RenderWindow window(sf::VideoMode(800, 600),
+                            "GenericGenetics - Moon Lander");
+    sf::View view(sf::Vector2f(400.0f, 300.0f), sf::Vector2f(400.0f, 300.0f));
+    window.setView(view);
+    auto map = buildMap();
 
     while (window.isOpen()) {
         sf::Event event;
@@ -9,9 +16,20 @@ int main() {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+
+            if (event.type == sf::Event::KeyReleased) {
+                if (event.key.code == sf::Keyboard::Escape || event.key.code == sf::Keyboard::Q) {
+                    window.close();
+                }
+            }
         }
 
         window.clear(sf::Color::Black);
+
+        for (const auto& line : map) {
+            window.draw(line.data(), 2, sf::Lines);
+        }
+
         window.display();
     }
 
