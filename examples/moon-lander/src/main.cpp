9402ba7875d02b50df04c7aabd1743de9ee74123
Axel Cocat <ax.cocat@gmail.com>
2020-04-03 03:19:13 +0200

    Added moonlander example with sfml blank window

---
+++ examples/moon-lander/src/main.cpp
@@ -0,0 +1,19 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+
+    return 0;
+}
